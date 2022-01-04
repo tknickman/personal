6c581f108d1584170ab0634e20815ff0658c8d22
Thomas Knickman <tknickman@gmail.com>
2022-01-04 12:23:12 -0500

    fix(config): fix tailwind reference

--- apps/info/tailwind.config.js
+++ apps/info/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./hooks/**/*.{js,ts,jsx,tsx}",
-    "./config.js",
+    "./config.{js,ts}",
   ],
   darkMode: "class",
   safelist: ["fill-current"],
