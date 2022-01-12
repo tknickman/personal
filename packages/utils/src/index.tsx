2f90ce1ca9377482e58b99f36321ef466fedcc1e
Thomas Knickman <tknickman@gmail.com>
2022-01-12 12:05:01 -0500

    chore(utils): move test utils (#19)

--- packages/utils/src/index.tsx
+++ packages/utils/src/index.tsx
@@ -2,6 +2,9 @@
 export { default as useFathom } from "./hooks/useFathom";
 export { default as useHostName } from "./hooks/useHostName";
 
+// test-utils
+export { default as createMockRouter } from "./test-utils/createMockRouter";
+
 // other
 export { default as getHostFromHostName } from "./getHostFromHostName";
 export { default as getDomains } from "./getDomains";
