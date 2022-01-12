1550255c5c5702c5e91861da8a01ec949b2953d5
Thomas Knickman <tknickman@gmail.com>
2022-01-12 11:48:47 -0500

    feat(apps/share): add embed analytics (#18)

--- apps/share/__tests__/index.test.tsx
+++ apps/share/__tests__/index.test.tsx
@@ -1,9 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import Home from "../pages/index";
+import { createMockRouter } from "../test-utils/createMockRouter";
+import { RouterContext } from "next/dist/shared/lib/router-context";
 
 describe("Home", () => {
   it("renders page heading", () => {
-    render(<Home />);
+    render(
+      <RouterContext.Provider value={createMockRouter({ query: {} })}>
+        <Home />
+      </RouterContext.Provider>
+    );
     const heading = screen.getByText("Public Asset Directory");
     expect(heading).toBeInTheDocument();
   });
